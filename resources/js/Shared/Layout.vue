67d4920e71abe32dc0130f3378ccdfb781aeac13
Ruben Steen <rubensteen123@gmail.com>
2022-06-15 20:00:07 +0000

    Added layouts

---
+++ resources/js/Shared/Layout.vue
@@ -0,0 +1,21 @@
+<template>
+
+    <Head title="Laravel loves Inertiajs" />
+
+    <h1>Layout</h1>
+
+    <slot />
+    
+</template>
+
+<script>
+import { Link } from '@inertiajs/inertia-vue3'
+import { Head } from '@inertiajs/inertia-vue3'
+
+export default {
+  components: {
+    Link,
+    Head
+  },
+}
+</script>
