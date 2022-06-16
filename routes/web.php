4004d29209ae07cfb49d12b164fed40a183a5fd1
Ruben Steen <rubensteen123@gmail.com>
2022-06-16 14:29:41 +0000

    Working on auth episode 23

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('login', [LoginController::class, 'create'])->name('login');
+Route::post('login', [LoginController::class, 'store']);
+Route::post('logout', [LoginController::class, 'destroy'])->middleware('auth');
+
 Route::get('/', function () {
     return inertia('Welcome', [
         'name' => 'World'
